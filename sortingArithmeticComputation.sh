2237f1e879e982473899f295d3d5dfc2c395c300
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-01-21 12:26:55 +0530

    Add: Compute c+a/b

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -1,8 +1,7 @@
-
 echo "Welcome to Sorting Arithmetic Computation Problem"
 
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
 echo "The three numbers are: $a $b $c"
-echo " $a * $b + $c = $(($a * $b + $c))"
+echo " $c + $a / $b = $(($c + $a / $b))"
